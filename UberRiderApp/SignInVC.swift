dd1f9df0c162de106096d03d8a78a1734d2ac9fb
WadeSellers <Waderoni@gmail.com>
2017-05-24 00:24:34 -0400

    setup loginVC UI

---
+++ UberRiderApp/SignInVC.swift
@@ -0,0 +1,29 @@
+//
+//  SignInVC.swift
+//  UberRiderApp
+//
+//  Created by Wade Sellers on 5/24/17.
+//  Copyright © 2017 WadeSellers. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func onLoginButtonTapped(_ sender: UIButton) {
+    }
+
+    @IBAction func onSignUpButtonTapped(_ sender: UIButton) {
+    }
+
+}
